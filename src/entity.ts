661b3f6c3e57ab98fec8206770f902a25a097b5b
Dmitry Pimonov <dimanheartilly@gmail.com>
2022-07-14 22:26:39 +0300

    Improvement: added cursor to entity.

--- src/entity.ts
+++ src/entity.ts
@@ -1,8 +1,17 @@
 import { PropertyName } from '@dipscope/type-manager';
+import { Cursor } from './cursor';
 
 /**
  * Type representing an entity.
  * 
  * @type {Entity}
  */
-export type Entity = Record<PropertyName, any>;
+export type Entity = Record<PropertyName, any> & 
+{
+    /**
+     * Optional cursor used for cursor based pagination.
+     * 
+     * @type {Cursor}
+     */
+    cursor?: Cursor
+};
