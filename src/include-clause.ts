2a409ffe1271695f99a6f4f17ec90d27a65eed31
dpimonov <dpimonov.webdev@gmail.com>
2023-06-19 22:20:44 +0200

    Bugfix: fix include collection command.

--- src/include-clause.ts
+++ src/include-clause.ts
@@ -1,5 +1,5 @@
 import { Entity } from './entity';
-import { EntityCollectionLike } from './entity-collection-like';
+import { EntityCollection } from './entity-collection';
 import { EntityInfoProxyRoot } from './entity-info-proxy';
 import { PropertyInfoProxy, PropertyInfoProxyRoot } from './property-info-proxy';
 
@@ -15,7 +15,7 @@ export type IncludeClause<TEntity extends Entity, TProperty extends Entity> = (e
  * 
  * @type {IncludeCollectionClause<TEntity, TProperty>}
  */
-export type IncludeCollectionClause<TEntity extends Entity, TProperty extends Entity> = (entityInfoProxyRoot: EntityInfoProxyRoot<TEntity>) => PropertyInfoProxy<EntityCollectionLike<TProperty>>;
+export type IncludeCollectionClause<TEntity extends Entity, TProperty extends Entity> = (entityInfoProxyRoot: EntityInfoProxyRoot<TEntity>) => PropertyInfoProxy<EntityCollection<TProperty>> | PropertyInfoProxy<Array<TProperty>>;
 
 /**
  * Represents a clause to build a child include expression for an entity.
@@ -29,4 +29,4 @@ export type ThenIncludeClause<TProperty extends Entity, TChildProperty extends E
  * 
  * @type {ThenIncludeCollectionClause<TEntity, TChildProperty>}
  */
-export type ThenIncludeCollectionClause<TProperty extends Entity, TChildProperty extends Entity> = (propertyInfoProxyRoot: PropertyInfoProxyRoot<TProperty>) => PropertyInfoProxy<EntityCollectionLike<TChildProperty>>;
+export type ThenIncludeCollectionClause<TProperty extends Entity, TChildProperty extends Entity> = (propertyInfoProxyRoot: PropertyInfoProxyRoot<TProperty>) => PropertyInfoProxy<EntityCollection<TChildProperty>> | PropertyInfoProxy<Array<TChildProperty>>;
